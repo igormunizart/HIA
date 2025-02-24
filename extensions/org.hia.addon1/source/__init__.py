58fe5546a09542caaf0dce50b9caebc21a60d1ba
Igor José Muniz <69331668+igormunizart@users.noreply.github.com>
2025-02-23 23:28:10 -0300

    Configuração inicial do repositório HIA
    Criação da estrutura básica para repositório de extensões do Blender

---
+++ extensions/org.hia.addon1/source/__init__.py
@@ -0,0 +1,12 @@
+bl_info = {
+    "name": "HIA Addon",
+    "author": "Seu Nome",
+    "version": (1, 0, 0),
+    "blender": (4, 3, 0)
+}
+
+def register():
+    print("HIA Addon registrado com sucesso!")
+
+def unregister():
+    print("HIA Addon desregistrado.")
