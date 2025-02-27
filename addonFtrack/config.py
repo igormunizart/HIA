fb47930e5cf6b469f10eb20a2c79bfef1df57d8a
Igor José Muniz <69331668+igormunizart@users.noreply.github.com>
2025-02-27 01:43:36 -0300

    Organização

--- addonFtrack/config.py
+++
@@ -1,9 +0,0 @@
-# Arquivo de configuração para credenciais do Ftrack
-
-# Configurações do servidor
-SERVER_URL = "https://histeria.ftrackapp.com"
-USERNAME = "igor@histeria.studio"
-
-# ATENÇÃO: Mantenha sua API key confidencial
-# Não compartilhe este arquivo ou faça commit em repositórios públicos
-API_KEY = "MWNlNjBkZDktMmQ0ZS00YjVlLWEwOTctZDE3MWI5ZjJiNTUzOjphNzNhMGZkZi1kNjM1LTRhZjItOGQzOS1lNThkNGNhNTE1Nzc"
